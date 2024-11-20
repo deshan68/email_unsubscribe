3af5165e9d85b5c3e6f2581d0240811f817e8854
deshan68 <arundeshan@gmail.com>
2024-11-20 08:24:59 +0530

    Chore: Add Github repo link

--- client/src/components/AboutSection.tsx
+++ client/src/components/AboutSection.tsx
@@ -1,11 +1,16 @@
 const AboutSection = () => {
+  const githubRepo = "https://github.com/deshan68/email_unsubscribe/tree/main";
+
   return (
     <div className="flex flex-col justify-center items-center w-[1000px] pt-10 pb-5">
       <h1 className="font-medium text-xl">About this project</h1>
       <p className="text-sm text-center text-[#8E8E93] py-2 px-52">
         This is an open-source project aimed at simplifying the process of
         managing unsubscribe links from emails. Feel free to contribute on
-        <button className="text-[#AF52DE] font-bold">
+        <button
+          className="text-[#AF52DE] font-bold"
+          onClick={() => window.open(githubRepo, "_blank")}
+        >
           &nbsp;GitHub&nbsp;
         </button>
         to enhance its functionality or fix issues.
